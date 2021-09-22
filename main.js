5e84a7b77956a62b112ef920939760e51c30b1b7
lucianets <ltsluciane@icloud.com>
2021-09-22 18:01:31 -0300

    commit on 'main'

---
+++ main.js
@@ -0,0 +1,316 @@
+var carta1 = {
+    nome: "Anakin Skywalker",
+    imagem: "https://64.media.tumblr.com/88c4bae811e4e5f7af08fe29b466df37/796ea77225a9bbbe-5c/s400x600/3ba3b3f2cdc361eac2c79d34c7e2c0a8b20cbbaf.png",
+    atributos:{
+      Ataque: 9,
+      Defesa: 8,
+      Força: 9
+    }
+  }
+  
+  var carta2 = {
+    nome: "Padmé Amidala", imagem: "https://64.media.tumblr.com/7ce0481faed32041f2634d21993a9c59/d44f87082639afd3-c6/s400x600/1e4e8cafbb0cee5fceae39d8df3474b3948eb5a8.jpg",
+    atributos:{
+      Ataque: 6,
+      Defesa: 4,
+      Força: 1,
+    }
+  }
+  
+  var carta3 = {
+    nome: "Leia Organa",
+    imagem: "https://64.media.tumblr.com/45b8f1fdbc83dc22871fec6b5d218b50/521b2354d5100621-6b/s1280x1920/70fcfbe12dcf453fd0e35c9127d421eeb10296ce.jpg",
+    atributos:{
+      Ataque: 7,
+      Defesa: 5,
+      Força: 5,
+    }
+  }
+  
+  var carta4 = {
+    nome: "Luke Skywalker",
+    imagem: "https://64.media.tumblr.com/024248b1df4cd4025f2ba486794c9c6b/fe73c87e93f95c6e-71/s640x960/4cdad913074ffdebb84b5189866cafeae0544c69.jpg",
+    atributos:{
+      Ataque: 7,
+      Defesa: 7,
+      Força: 8,
+    }
+  }
+  
+  var carta5 = {
+    nome: "Ben Solo",
+    imagem: "https://64.media.tumblr.com/30a21bea99b745315b36f6bfc449b7ed/e86bcf9b128c9625-38/s400x600/20cb5d4c4e8d57aeaa7f5f7f297d9f85dbac0ad2.png",
+    atributos:{
+      Ataque: 8,
+      Defesa: 5,
+      Força: 7,
+    }
+  }
+  
+  var carta6 = {
+    nome: "Han Solo",
+    imagem: "https://64.media.tumblr.com/f860a8532068d83f38fbb7fd2670411b/0e58088653f6a7be-ae/s1280x1920/8e2a8021dad68791d32f2c185e1517e1b9ef5d00.jpg",
+    atributos:{
+      Ataque: 5,
+      Defesa: 7,
+      Força: 0,
+    }
+  }
+  
+  var carta7 = {
+    nome: "Kylo Ren",
+    imagem:  "https://64.media.tumblr.com/a74a3cd69cd1b0e341652a32f5bdffc6/1a929111cf54cf2e-0e/s250x400/5c74344dca39c723a9e45d2dd732348d8881074d.png",
+    atributos:{
+      Ataque: 5,
+      Defesa: 7,
+      Força: 9,
+    }
+  }
+  
+  var carta8 = {
+    nome: "Rey P. Skywalker",
+    imagem: "https://64.media.tumblr.com/caa91291ec0256f3576ea4d72837dae4/7537081d1d3df7ae-c9/s1280x1920/4798a7d4539cfd343d736f84f366023001567b2c.jpg",
+    atributos:{
+      Ataque: 6,
+      Defesa: 6,
+      Força: 8,
+    }
+  }
+  
+  var carta9 = {
+    nome: "Darth Vader",
+    imagem: "https://64.media.tumblr.com/5fba45a17ef8fd3db72e8f0ba4133cc3/d44f87082639afd3-6c/s400x600/bd37cb552411b1297c99077563c1b009e6309713.jpg",
+    atributos:{
+      Ataque: 9,
+      Defesa: 8,
+      Força: 10
+    }
+  }
+
+  var carta10 = {
+    nome: "Chewbacca",
+    imagem: "https://64.media.tumblr.com/35d807710ec7d90d48281d052b8fde7f/cf4dddc957dbc54f-fd/s1280x1920/aa40e2365d133e622ba07083928b03c6319769d3.jpg",
+    atributos:{
+      Ataque: 5,
+      Defesa: 3,
+      Força: 0,
+    }
+  }
+
+  var carta11 = {
+    nome: "Obi Wan Kenobi",
+    imagem: "https://64.media.tumblr.com/ab211663ebb817c53f42a0485f76b84e/b6125a0cf68a9ea8-fd/s1280x1920/70cf1f79bf9d1b3ed8990af2c6f58273801854a8.jpg",
+    atributos:{
+      Ataque: 5,
+      Defesa: 7,
+      Força: 8,
+    }
+  }
+
+  var carta12 = {
+    nome: "Ahsoka Tano",
+    imagem: "https://64.media.tumblr.com/3fb110ebc4875de5dd8d6502babcd80a/9dcf722449d318e3-a2/s1280x1920/fc95f23cc29081412762e7d28d98dbc63d9eacf7.jpg",
+    atributos:{
+      Ataque: 8,
+      Defesa: 6,
+      Força: 7,
+    }
+  }
+
+  var carta13 = {
+    nome: "Qui- Gon Jinn",
+    imagem: "https://64.media.tumblr.com/e9fc868ebfa6613fbb0c45fe7fad8aae/5279e6c38a2395a0-c7/s400x600/9e5d6dca8ffd9aa7dc4188a16226d7324c7cd21d.png",
+    atributos:{
+      Ataque: 5,
+      Defesa: 8,
+      Força: 7,
+    }
+  }
+
+  var carta14 = {
+    nome: "Shmi Skywalker",
+    imagem: "https://64.media.tumblr.com/b49b4ed01e32d5082d93991ba48a3139/tumblr_o6w1akxgZk1rsrzfmo1_1280.jpg",
+    atributos:{
+      Ataque: 0,
+      Defesa: 0,
+      Força: 0,
+    }
+  }
+ 
+  var carta15 = {
+    nome: "C- 3PO",
+    imagem: "https://preview.redd.it/uqpj8n3dnqi11.gif?format=png8&s=7655aa7043f5af6b155014e4fda0241713c0ff90",
+    atributos:{
+      Ataque: 1,
+      Defesa: 2,
+      Força: 0,
+    }
+  }
+
+  var carta16 = {
+    nome: "R2-D2",
+    imagem: "https://64.media.tumblr.com/610bed4d69b30b2ccc788f03f9560d2f/81ed02144af81083-7c/s400x600/5354a3e5d8d2e240134230fcb4f34339ecea2a69.jpg",
+    atributos:{
+      Ataque: 5,
+      Defesa: 5,
+      Força: 0,
+    }
+  }
+var cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14, carta15, carta16];
+  
+var cartaMaquina = 0;
+var cartaJogador = 0;
+var pontosJogador = 0;
+var pontosMaquina = 0;
+
+function sortearCarta() {
+  var numeroAleatorioMaquina = parseInt(Math.random() * cartas.length);
+  var numeroAleatorioJogador = parseInt(Math.random() * cartas.length);
+
+  while (numeroAleatorioMaquina == numeroAleatorioJogador) {
+    numeroAleatorioJogador = parseInt(Math.random() * cartas.length);
+  }
+
+  cartaJogador = cartas[numeroAleatorioJogador];
+  cartas.splice(numeroAleatorioJogador, 1);
+  cartaMaquina = cartas[numeroAleatorioMaquina];
+  cartas.splice(numeroAleatorioMaquina, 1);
+
+  
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+
+  
+  exibeCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName("atributo");
+  
+  for (var i = 0; i < radioAtributos.length; i++) {
+
+    if (radioAtributos[i].checked == true) {
+  
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado(); 
+  var elementoResultado = document.getElementById("resultado");
+  var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+  var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+  
+  if (valorCartaJogador > valorCartaMaquina) {
+    pontosJogador++;
+    elementoResultado.innerHTML = "Você venceu";
+  } else if (valorCartaJogador < valorCartaMaquina) {
+    elementoResultado.innerHTML = "Você perdeu <br>A carta do seu inimigo é maior";
+    pontosMaquina++;
+  } else {
+    elementoResultado.innerHTML = "Empatou";
+    pontosJogador++;
+    pontosMaquina++;
+  
+  }
+  exibeCartaMaquina();
+
+  document.getElementById("btnJogar").disabled = true;
+  document.getElementById("btnProxima").disabled = false;
+
+  
+  pontuacao();
+
+  if (pontosJogador >= 3) {
+    elementoResultado.innerHTML = "Parabéns você venceu o jogo,que a Força esteja com você!!";
+    document.getElementById("btnJogar").disabled = true;
+    document.getElementById("btnProxima").disabled = true;
+    document.getElementById("btnSortear").disabled = true;
+  }
+  if (pontosMaquina >= 3) {
+    elementoResultado.innetHTML = "Que triste você perdeu o jogo...Eu sugiro uma nova estratégia, R2, deixe o Wookie ganhar!";
+    document.getElementById("btnJogar").disabled = true;
+    document.getElementById("btnProxima").disabled = true;
+    document.getElementById("btnSortear").disabled = true;
+  }
+}
+
+function proximaRodada() {
+ 
+  var resultadoBranco = document.getElementById("resultado");
+  resultadoBranco.innerHTML = "";
+  var jogadorBranco = document.getElementById("carta-jogador");
+  jogadorBranco.innerHTML = "";
+  var maquinaBranco = document.getElementById("carta-maquina");
+  maquinaBranco.innerHTML = "";
+  var divCartas = document.getElementById("cartas");
+  divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div> <div id="carta-maquina" class="carta"></div>`;
+
+  document.getElementById("btnSortear").disabled = false;
+  document.getElementById("btnJogar").disabled = true;
+  document.getElementById("btnProxima").disabled = true;
+}
+
+function exibeCartaJogador() {
+  var exibeCarta = document.getElementById("carta-jogador");
+  
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+  var opcoesTexto = "";
+  
+  var moldura =
+    '<img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/a1c23559-70b8-439c-83a6-eaaa9d85285b/180-1809947_movie-ticket-clipart.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210922%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210922T173735Z&X-Amz-Expires=86400&X-Amz-Signature=42bc5d9ee5f37e8c0bcbd2e2e56a81a9188ab218fde33cb61b7cdea63a3f3c1f&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22180-1809947_movie-ticket-clipart.png%22" style=" width: inherit; height: inherit; position: absolute;">';
+  
+  exibeCarta.style.backgroundImage = `url(${cartaJogador.imagem})`;
+
+  
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' checked value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+
+
+  var html = "<div id='opcoes' class='carta-status'>";
+
+  exibeCarta.innerHTML = moldura + nome + html + opcoesTexto + "</div>";
+}
+
+function exibeCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+  var opcoesTexto = "";
+  var moldura =
+    '<img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/a1c23559-70b8-439c-83a6-eaaa9d85285b/180-1809947_movie-ticket-clipart.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAT73L2G45O3KS52Y5%2F20210922%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20210922T173735Z&X-Amz-Expires=86400&X-Amz-Signature=42bc5d9ee5f37e8c0bcbd2e2e56a81a9188ab218fde33cb61b7cdea63a3f3c1f&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22180-1809947_movie-ticket-clipart.png%22" style=" width: inherit; height: inherit; position: absolute;">';
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " +
+      cartaMaquina.atributos[atributo] +
+      "<br>";
+  }
+
+  var html = "<div id='opcoes' class='carta-status --spacing'>";
+
+  divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + "</div>";
+}
+
+function pontuacao() {
+  var pontos = document.getElementById("pontos");
+  pontos.innerHTML =
+    "Jogador " + pontosJogador + "/" + pontosMaquina + " Máquina";
+}
+
+function reiniciarJogo() {
+  proximaRodada();
+  var resultadoBranco = document.getElementById("pontos");
+  resultadoBranco.innerHTML = "";
+  pontosMaquina = 0;
+  pontosJogador = 0;
+}
